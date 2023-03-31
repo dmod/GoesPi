427d3d05c02bfdcde127b29de799a6292d42dd1b
dmod <dmod>
2023-03-31 00:56:47 -0400

    Pushing trackbar to bottom

--- script.js
+++ script.js
@@ -1,48 +1,60 @@
-getAllImages = function (endpoint) {
+window.onload = function () {
+  // Get the slider container and trackbar elements
+  var sliderContainer = document.getElementById("slider-container");
+  var sliderTrackbar = document.getElementById("slider-trackbar");
 
+  // Get the directory listing and extract the image URLs
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', endpoint, true);
+  xhr.open('GET', 'images/goes16/fd/fc/2023-03-31/', true);
   xhr.onload = function () {
-
-    var imageRegex = /href="(.+\.jpg)"/g;
+    // Extract the image URLs from the directory listing
+    var regex = /href="(.+\.jpg)"/g;
     var match;
     var imageList = [];
-    while ((match = imageRegex.exec(xhr.responseText)) !== null) {
-      imageList.push(endpoint + match[1]);
+    while ((match = regex.exec(xhr.responseText)) !== null) {
+      imageList.push('images/goes16/fd/fc/2023-03-31/' + match[1]);
     }
 
-    var dirRegex = /href="(.+\/)"/g;
-    var match;
-    var dirList = [];
-    while ((match = dirRegex.exec(xhr.responseText)) !== null) {
-      dirList.push(match[1]);
+    // Preload and add the images to the slider container
+    for (var i = 0; i < imageList.length; i++) {
+      var container = document.createElement("div");
+      container.classList.add("slider-image-container");
+
+      var img = new Image();
+      img.src = imageList[i];
+
+      img.width = 2712
+      img.height = 2712
+
+      img.onload = (function (container, img, filename) {
+        return function () {
+          container.appendChild(img);
+
+          //var text = document.createElement("div");
+          //text.classList.add("slider-image-text");
+          //text.innerText = filename;
+          //container.appendChild(text);
+
+          sliderContainer.appendChild(container);
+          sliderTrackbar.max = imageList.length - 1;
+
+          sliderTrackbar.value = 0
+          sliderTrackbar.dispatchEvent(new Event("input"))
+
+        }
+      })(container, img, imageList[i].split('/').pop());
     }
-
-    dirList = dirList.filter(image => image !== '../')
-
-    dirList.forEach(x => getAllImages(endpoint + x));
-
-    imageList.forEach(image => {
-
-      console.log(image)
-
-      var newHTMLImag = document.createElement('img');
-      newHTMLImag.src = image
-      newHTMLImag.width = 500
-      newHTMLImag.height = 500
-
-      document.getElementById('controls-and-images').appendChild(newHTMLImag);
-    }
-
-    )
 
   };
   xhr.send();
 
-}
-
-window.onload = function () {
-
-  getAllImages('images/goes16/fd/fc/2023-03-31/')
-
+  // Add event listener for the trackbar to update the active image
+  sliderTrackbar.addEventListener("input", function () {
+    var activeImage = sliderContainer.querySelector("img.active");
+    if (activeImage) {
+      activeImage.classList.remove("active");
+    }
+    var newActiveImage = sliderContainer.querySelectorAll("img")[this.value];
+    newActiveImage.classList.add("active");
+  });
 };
